21195416f4dc1f08bbbb2a2ccdf4c19762cfbecf
josephiskingkong <iloveuyungcole@gmail.com>
2023-02-13 13:41:09 +0700

    I changed the main loop

--- first-task.cs
+++ first-task.cs
@@ -6,24 +6,19 @@ namespace Calculator
   {
     static void Main(string[] argv)
     {
-      int A; int N; int OldN; int OldA;
+      int A; int N;
 
       Console.WriteLine("Enter the number: ");
       A = Convert.ToInt32(Console.ReadLine());
-      OldA = A;
 
       Console.WriteLine("Enter the number degree: ");
       N = Convert.ToInt32(Console.ReadLine());
 
-      OldN = N;
-
-      while (N > 1)
-      {
-        A = A * OldA;
-        N = N - 1;
+      for(int i=1; i<N; i++){
+        A *= A; // A = A * A
       }
 
-      Console.WriteLine($"{OldA} in {OldN} degree = {A}");
+      Console.WriteLine($"{A} in {N} degree = {A}");
       Console.ReadKey();
     }
   }
